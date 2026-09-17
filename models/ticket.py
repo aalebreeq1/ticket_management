dc3fb12313c8b79a8d243435de111db23b79fa49
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-17 19:15:01 +0300

    did what abdulla said to me to optimize the module

--- models/ticket.py
+++ models/ticket.py
@@ -12,11 +12,19 @@ class TicketTicket(models.Model):
         readonly=True,
         tracking=True,
         copy=False,
+        index=True,
         default=lambda self: _("New"),
     )
-    title = fields.Char(string="Ticket Title", tracking=True)
+    title = fields.Char(string="Ticket Title", required=True, tracking=True)
     description = fields.Html(string="Description")
-    partner_id = fields.Many2one("res.partner", string="Customer", tracking=True)
+    partner_id = fields.Many2one("res.company", string="Company", tracking=True)
+    
+    responsible_manager_id = fields.Many2one(
+        "res.users",
+        string="Responsible Manager",
+        tracking=True,
+        default=lambda self: self.env.user,
+    )
     user_id = fields.Many2one(
         "res.users",
         string="Assigned To",
@@ -26,19 +34,33 @@ class TicketTicket(models.Model):
     category_id = fields.Many2one("ticket.category", string="Category")
     tag_ids = fields.Many2many("ticket.tag", string="Tags")
 
-    # approval_user_ids = fields.Many2many("res.users", string="Approval Users")
+    approval_user_ids = fields.Many2many("res.users", string="Approval Users")
+    
     priority = fields.Selection(
         [("0", "Low"), ("1", "Normal"), ("2", "High"), ("3", "Very High")],
         string="Priority",
         default="1",
         tracking=True,
     )
-    
+
+    # @api.model
+    # def _get_manager_domain(self):
+    #     manager_group = self.env.ref('ticket_management.group_ticket_manager', raise_if_not_found=False)
+    #     if manager_group:
+    #         return [('groups_id', 'in', manager_group.id)]
+    #     return []
+
+    # @api.model
+    # def _get_user_domain(self):
+    #     user_group = self.env.ref('ticket_management.group_ticket_user', raise_if_not_found=False)
+    #     if user_group:
+    #         return [('groups_id', 'in', user_group.id)]
+    #     return []
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if vals.get("ticket_ref", _("New")) == _("New"):
+            if not vals.get("ticket_ref") or vals.get("ticket_ref") == _("New"):
                 vals["ticket_ref"] = self.env["ir.sequence"].next_by_code(
                     self._name
                 ) or _("New")
